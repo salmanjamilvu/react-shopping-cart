2fbe7441f3b82dc0ec2acdc7b14b2de00afb0d34
salmanjamilvu <salmanjamilvu@gmail.com>
2021-04-25 01:46:05 +0500

    create header and footer

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>
+        Product List
+      </main>
+      <footer>
+        All right is reserved.      
+      </footer>
     </div>
   );
 }
