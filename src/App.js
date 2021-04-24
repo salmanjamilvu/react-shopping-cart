1d1d968e9fd35e736c09ac1fda1b4c69553f70a4
salmanjamilvu <salmanjamilvu@gmail.com>
2021-04-25 02:38:09 +0500

    Create Products components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import data from './Data.json'
+import Products from './components/products/Products'
 
 function App() {
   return (
@@ -7,7 +9,14 @@ function App() {
         <a href="/">React Shopping Cart</a>
       </header>
       <main>
-        Product List
+        <div className="content">
+          <div className="main">
+            <Products products={data.products} />
+          </div>
+          <div className="sidebar">
+            Sidebar
+          </div>
+        </div>
       </main>
       <footer>
         All right is reserved.      
